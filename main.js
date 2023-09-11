7f4b86500641c2692ffff50b2bcf107bb7267587
grigoryan.m <waldm4rk@gmail.com>
2023-09-11 12:19:18 +0400

    finished project

---
+++ main.js
@@ -0,0 +1,202 @@
+const champions = [
+    "Aatrox.png",
+    "Ahri.png",
+    "Akali.png",
+    "Akshan.png",
+    "Alistar.png",
+    "Amumu.png",
+    "Anivia.png",
+    "Annie.png",
+    "Aphelios.png",
+    "Ashe.png",
+    "AurelionSol.png",
+    "Azir.png",
+    "Bard.png",
+    "Belveth.png",
+    "Blitzcrank.png",
+    "Brand.png",
+    "Braum.png",
+    "Caitlyn.png",
+    "Camille.png",
+    "Cassiopeia.png",
+    "Chogath.png",
+    "Corki.png",
+    "Darius.png",
+    "Diana.png",
+    "Draven.png",
+    "DrMundo.png",
+    "Ekko.png",
+    "Elise.png",
+    "Evelynn.png",
+    "Ezreal.png",
+    "Fiddlesticks.png",
+    "Fiora.png",
+    "Fizz.png",
+    "Galio.png",
+    "Gangplank.png",
+    "Garen.png",
+    "Gnar.png",
+    "Gragas.png",
+    "Graves.png",
+    "Gwen.png",
+    "Hecarim.png",
+    "Heimerdinger.png",
+    "Illaoi.png",
+    "Irelia.png",
+    "Ivern.png",
+    "Janna.png",
+    "JarvanIV.png",
+    "Jax.png",
+    "Jayce.png",
+    "Jhin.png",
+    "Jinx.png",
+    "Kaisa.png",
+    "Kalista.png",
+    "Karma.png",
+    "Karthus.png",
+    "Kassadin.png",
+    "Katarina.png",
+    "Kayle.png",
+    "Kayn.png",
+    "Kennen.png",
+    "Khazix.png",
+    "Kindred.png",
+    "Kled.png",
+    "KogMaw.png",
+    "KSante.png",
+    "Leblanc.png",
+    "LeeSin.png",
+    "Leona.png",
+    "Lillia.png",
+    "Lissandra.png",
+    "Lucian.png",
+    "Lulu.png",
+    "Lux.png",
+    "Malphite.png",
+    "Malzahar.png",
+    "Maokai.png",
+    "MasterYi.png",
+    "Milio.png",
+    "MissFortune.png",
+    "Mordekaiser.png",
+    "Morgana.png",
+    "Naafiri.png",
+    "Nami.png",
+    "Nasus.png",
+    "Nautilus.png",
+    "Neeko.png",
+    "Nidalee.png",
+    "Nilah.png",
+    "Nocturne.png",
+    "Nunu.png",
+    "Olaf.png",
+    "Orianna.png",
+    "Ornn.png",
+    "Pantheon.png",
+    "Poppy.png",
+    "Pyke.png",
+    "Qiyana.png",
+    "Quinn.png",
+    "Rakan.png",
+    "Rammus.png",
+    "RekSai.png",
+    "Rell.png",
+    "Renata.png",
+    "Renekton.png",
+    "Rengar.png",
+    "Riven.png",
+    "Rumble.png",
+    "Ryze.png",
+    "Samira.png",
+    "Sejuani.png",
+    "Senna.png",
+    "Seraphine.png",
+    "Sett.png",
+    "Shaco.png",
+    "Shen.png",
+    "Shyvana.png",
+    "Singed.png",
+    "Sion.png",
+    "Sivir.png",
+    "Skarner.png",
+    "Sona.png",
+    "Soraka.png",
+    "Swain.png",
+    "Sylas.png",
+    "Syndra.png",
+    "TahmKench.png",
+    "Taliyah.png",
+    "Talon.png",
+    "Taric.png",
+    "Teemo.png",
+    "Thresh.png",
+    "Tristana.png",
+    "Trundle.png",
+    "Tryndamere.png",
+    "TwistedFate.png",
+    "Twitch.png",
+    "Udyr.png",
+    "Urgot.png",
+    "Varus.png",
+    "Vayne.png",
+    "Veigar.png",
+    "Velkoz.png",
+    "Vex.png",
+    "Vi.png",
+    "Viego.png",
+    "Viktor.png",
+    "Vladimir.png",
+    "Volibear.png",
+    "Warwick.png",
+    "Wukong.png",
+    "Xayah.png",
+    "Xerath.png",
+    "XinZhao.png",
+    "Yasuo.png",
+    "Yone.png",
+    "Yorick.png",
+    "Yuumi.png",
+    "Zac.png",
+    "Zed.png",
+    "Zeri.png",
+    "Ziggs.png",
+    "Zilean.png",
+    "Zoe.png",
+    "Zyra.png"
+]
+
+let file, password;
+const bluredImage = document.getElementById("iconImg")
+,submitButton = document.getElementById("submitButton")
+,inputField = document.getElementById("inputField");
+
+function getRandomFromArray(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+}
+function getPassword(){
+    file = getRandomFromArray(champions);
+    password = file.slice(0, -4);
+    bluredImage.src = "/src/" + file;
+}
+document.addEventListener("DOMContentLoaded", ()=>{
+    getPassword();
+});
+submitButton.addEventListener("click", checkAnswer);
+document.addEventListener("keydown", (event) => {
+    if(event.key === "Enter"){
+        event.preventDefault();
+        checkAnswer();
+    }
+});
+
+function checkAnswer(){
+    if(inputField.value.toLowerCase() === password.toLowerCase()){
+        alert("You won!");
+    }else{
+        alert("You lost! Champion was " + password);
+    }
+    location.reload();
+    inputField.value = '';
+    inputField.focus();
+}
